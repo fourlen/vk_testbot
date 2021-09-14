46b6c8ee36513c122f2a2aa9fab5007659ea2c7c
fourlen <oborin.2013@mail.ru>
2021-09-14 20:36:00 +0300

    added confirmation

--- flaskapp.py
+++ flaskapp.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+import json
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return 'ok'
-
+    values = request.json
+    if values['type'] == 'confirmation' and values['group_id'] == 198161648:
+        return 'e571d5fa' 
 
 if __name__ == '__main__':
     app.run()
